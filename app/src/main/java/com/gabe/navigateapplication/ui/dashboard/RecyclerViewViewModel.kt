e5ef0d72436559265130b3671f85f97ece37c516
gabeli <hugegear@gmail.com>
2022-06-15 19:11:08 +0800

    fix(bug)Loading data from the network when returning due to creating an instance of Page

--- app/src/main/java/com/gabe/navigateapplication/ui/dashboard/RecyclerViewViewModel.kt
+++ app/src/main/java/com/gabe/navigateapplication/ui/dashboard/RecyclerViewViewModel.kt
@@ -1,6 +1,5 @@
 package com.gabe.navigateapplication.ui.dashboard
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -10,17 +9,39 @@ import androidx.paging.cachedIn
 import com.gabe.navigateapplication.network.CharacterData
 import com.gabe.navigateapplication.network.RetroService
 import com.gabe.navigateapplication.pagingsource.CharacterPagingSource
+import dagger.hilt.android.scopes.ActivityScoped
 import kotlinx.coroutines.flow.Flow
 
-class RecyclerViewViewModel( private val retroService: RetroService) : ViewModel() {
+
+@ActivityScoped
+class RecyclerViewViewModel(private val retroService: RetroService) : ViewModel() {
 //    var retroService: RetroService = RetroInstance.getRetroInstance().create(RetroService::class.java)
 
+//    private lateinit var pageDataFlow  :Flow<PagingData<CharacterData>>
+
+
+    //    fun loadData() {
+//        val id = handle["id"] ?: "default"
+//        viewModelScope.launch {
+//            val response = retroService.getDataFromApi(id)
+//            // Handle response
+//        }
+//    }
+//    private lateinit var pageDataFlow  : MutableSharedFlow<PagingData<CharacterData>>
+    private var pageDataFlow: Flow<PagingData<CharacterData>>? = null
 
     fun getListData(): Flow<PagingData<CharacterData>> {
-        return Pager(
+        //避免每次都创建新的实 例，导 致回不到 上次加载的状态。
+        val lastResult = pageDataFlow
+        if (lastResult != null) {
+            return lastResult
+        }
+        pageDataFlow = Pager(
             //这里不要限制最大数，当记录超最大数时，超的会空指针。除非你有把制握不超。
-            config = PagingConfig(pageSize = 20 ),//PagingConfig(pageSize = 34, maxSize = 200),
+            config = PagingConfig(pageSize = 20),//PagingConfig(pageSize = 34, maxSize = 200),
             pagingSourceFactory = { CharacterPagingSource(retroService) }
         ).flow.cachedIn(viewModelScope)
+
+        return pageDataFlow as Flow<PagingData<CharacterData>>
     }
 }
