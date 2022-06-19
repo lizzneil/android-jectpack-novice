5bf2ab608a4f436aaa83249f6311fa5c3cd96acd
gabeli <hugegear@gmail.com>
2022-06-19 10:28:22 +0800

    refactor(GlideModel):Optimized with the new version of GlideModel

--- app/src/main/java/com/gabe/navigateapplication/glide/GlideConfig.kt
+++ app/src/main/java/com/gabe/navigateapplication/glide/GlideConfig.kt
@@ -16,6 +16,15 @@ import com.gabe.navigateapplication.R
 //2。  Keeping a GlideModule ////add your implementation class to your proguard.cfg/proguard.pro file to allow your module to be instantiated via reflection.
 //3。Registering a GlideModule /////Finally add a meta-data tag to your AndroidManifest.xml file so Glide can find your module:
 //other： Conflicting GlideModules   排除错误的///<meta-data android:name="com.mypackage.MyGlideModule" tools:node="remove" />
+
+//append：
+//
+// Glide 4.0 need not have declare "GlideModule" in AndroidManifest.xml. You just need to following steps:
+//
+//YourGlideModule extends AppGlideModule, you can override function applyOptions in the YourGlideModule class.
+//You should make project in "android studio -> build -> make project", it will generate the GlideApp class.
+//Use the GlideApp such as GlideApp.with(this).load(imgUrl).into(glide_test_iv1)
+
 @GlideModule
 class GlideConfig : AppGlideModule() {
 
@@ -33,11 +42,14 @@ class GlideConfig : AppGlideModule() {
     }
 
 
-    //运行阶段，实例化glide时，报这个错，暂时这样解，但感觉这个方法不好。看源码看出来的。不是正规路线。
+    //1，运行阶段，实例化glide时，报这个错，暂时这样解，但感觉这个方法不好。看源码看出来的。不是正规路线。
     //Fatal Exception: java.lang.RuntimeException: Expected instanceof GlideModule, but found:
-    override fun isManifestParsingEnabled(): Boolean {
-        return false
-    }
+//2，APPEND；   高版本不需要在manifest里声明。所以不需要分析。 换高版本就没事了。
+//    override fun isManifestParsingEnabled(): Boolean {
+//        return false
+//    }
+
+
 //    override fun applyOptions(context: Context, builder: GlideBuilder) {
 //        // Default empty impl.
 //    }
